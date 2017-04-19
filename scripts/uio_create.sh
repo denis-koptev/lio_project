01a0b059032e041afd9ef21888b0c8f4a85aaa91
denis-koptev <koptevda@ya.ru>
2017-04-19 15:10:28 -0700

    UIO creation script

---
+++ scripts/uio_create.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ $# -ne 3 ]
+then echo "Wrong number of arguments. Must be 3: HBA name, storage object name, size"
+     exit
+fi
+
+# maybe it's quite shitty
+#rm -rf /sys/kernel/config/target/core/user_* 
+
+mkdir -p "/sys/kernel/config/target/core/$1/$2"
+
+cd /sys/kernel/config/target/core/$1/$2
+echo -n dev_size=$3 > control
+echo -n dev_config=baz/addl_info_for_baz_handler > control
+echo -n 1 > enable
