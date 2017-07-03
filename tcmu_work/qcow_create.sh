f421eca079867e46000e9ef3515592f70bc243e5
denis-koptev <koptevda@ya.ru>
2017-07-03 14:19:49 +0300

    Blanks for other handlers added

--- tcmu_work/qcow_create.sh
+++ tcmu_work/qcow_create.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
+echo "Script not ready yet"
+exit
+
 if [ $# -ne 3 ]
 then echo "Wrong number of arguments. Must be 3: HBA name, storage object name, size"
      exit
 fi
 
-# maybe insert deletion of old versions?
-
 mkdir -p "/sys/kernel/config/target/core/$1/$2"
 
 cd /sys/kernel/config/target/core/$1/$2
 echo -n dev_size=$3 > control
-echo -n dev_config=file/test > control # path to some handler?
+echo -n dev_config=qcow/$2 > control 
 echo -n 1 > enable
+
+echo "Your backstore directory: $1/$2"
---
+++ tcmu_work/rbd_create.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "Script not ready yet"
+exit
+
+if [ $# -ne 3 ]
+then echo "Wrong number of arguments. Must be 3: HBA name, storage object name, size"
+     exit
+fi
+
+mkdir -p "/sys/kernel/config/target/core/$1/$2"
+
+cd /sys/kernel/config/target/core/$1/$2
+echo -n dev_size=$3 > control
+echo -n dev_config=rbd/$2 > control 
+echo -n 1 > enable
+
+echo "Your backstore directory: $1/$2"
