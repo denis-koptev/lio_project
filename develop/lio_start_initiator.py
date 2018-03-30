3467c7f72008fbfc44e9e8fd2712d3d337b11bb8
denis-koptev <koptevda@ya.ru>
2018-03-30 07:28:48 -0700

    Registering and login processes added for initiator

---
+++ develop/lio_start_initiator.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import json
+
+
+if len(sys.argv) != 2:
+    print('[ERROR] You need to specify /path/to/config.json')
+    sys.exit(1)
+
+if not os.path.isfile(sys.argv[1]):
+    print('[ERROR] Config file not found')
+    sys.exit(1)
+
+config_file = open(sys.argv[1], 'r')
+config = json.load(config_file)
+
+print('[INFO] Creating initiator with the following config:')
+print(json.dumps(config, indent=4))
+
+iqn = config['iqn']
+
+if not os.path.isfile('/etc/iscsi/initiatorname.iscsi'):
+    print('[ERROR] No initiatorname file found. You should install open-iscsi.')
+    sys.exit(1)
+
+initiatorname = open('/etc/iscsi/initiatorname.iscsi', 'w')
+initiatorname.write('InitiatorName=' + iqn + '\n')
+initiatorname.close()
+
+print('[INFO] Successfully registered initiator IQN')
+
---
+++ develop/lio_start_initiator.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# Arg: path/to/config.json
+
+if [ "$(id -u)" != "0" ]; then
+    echo "[ERROR] This script must be run as root" 1>&2
+    exit 1
+fi
+
+if [ "$#" -ne 1 ]; then
+    echo "[ERROR] Invalid count of arguments"
+    echo "[INFO] Usage (as root): ./lio_start_initiator.sh /path/to/config.json"
+    exit 1
+fi
+
+# Registering initiator IQN
+python3 lio_start_initiator.py $1
+
+if [ $? -ne 0 ]; then
+    echo "[ERROR] Failed to register Initiator IQN"
+    exit 1
+fi
+
+echo "[INFO] Attempting to descover targets and log in"
+
+TARGET_IP=`cat session/target_ip`
+
+iscsiadm -m discovery -t sendtargets -p $TARGET_IP
+
+if [ $? -ne 0 ]; then
+    echo "[ERROR] Failed to discover targets"
+    exit 1
+fi
+
+iscsiadm -m node --login -p $TARGET_IP:3260
+
+if [ $? -ne 0 ]; then
+    echo "[ERROR] Failed to log in to target"
+    exit 1
+fi
+
+lsscsi
+
