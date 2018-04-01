f5bc4887a66a5d180ae0fec2c17509eb1e4230f8
Denis Koptev <koptevda@ya.ru>
2018-04-01 20:01:31 +0300

    Device parser added

---
+++ develop/parse_dev.py
@@ -0,0 +1,23 @@
+import re
+
+# FIND_DEV SCRIPT
+# Parses file with device list using regular expressions
+# Returns LUN and device name in /dev
+
+
+def parse_lio_dev(filename):
+    re_lun = r'(\d+(?=].*LIO))'
+    re_any = r'.*'
+    re_dev = r'((?<=/dev/)\S+)'
+
+    pattern = re.compile(re_lun + re_any + re_dev)
+
+    with open(filename, 'r') as dev_file:
+        data = dev_file.read()
+
+    devices = []
+
+    for dev in pattern.findall(data):
+        devices.append({'lun':dev[0],'dev':dev[1]})
+
+    return devices
