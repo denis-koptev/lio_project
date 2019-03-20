f92d2582112740b61c66af988c821c9fe0a93ce3
denis-koptev <koptevda@ya.ru>
2019-03-20 15:22:45 -0700

    [RUN_IO] Code refactored according to PyLint

--- run_io.py
+++ run_io.py
@@ -14,13 +14,15 @@ import sys
 import time
 import json
 import argparse
-from logger import Logger
+from lio_project.logger import Logger
 
 
 LOG = Logger()
 
 
 def parse_args():
+    """ Parses arguments given to STDIN using default argparse module """
+
     parser = argparse.ArgumentParser()
     parser.add_argument('dev_list', help='path to a file with list of devices from lsscsi call')
     parser.add_argument('init_config', help='path to an interanl initiator JSON config')
@@ -31,6 +33,8 @@ def parse_args():
 
 
 def get_json_from_file(path):
+    """ Reads JSON configuration file from a given path """
+
     if not os.path.isfile(path):
         LOG.error('JSON config for initiator not found')
         sys.exit(1)
@@ -42,6 +46,8 @@ def get_json_from_file(path):
 
 
 def parse_lio_dev(filename):
+    """ Parses SCSI devices in a file from a given path """
+
     re_lun = r'(\d+(?=].*LIO))'
     re_any = r'.*'
     re_dev = r'((?<=/dev/)\S+)'
@@ -59,43 +65,55 @@ def parse_lio_dev(filename):
     return devices
 
 
-def write_random(dev_path, size, bs):
+def write_random(dev_path, size, block_size):
+    """ Executes random WRITE IO operations
+
+    @dev_path     Path to a SCSI device to write to
+    @size         Size of IO in bytes
+    @block_size   Size of one IO block in bytes
+
+    @ret          Dict with information about IO
+
+    """
+
     success = 1
     message = 'OK'
     total_time = 0
     speed = 0
 
-    if bs > size:
+    if block_size > size:
         LOG.warning('Specified block size is larger that total io size. Reducing')
-        bs = size
+        block_size = size
         message = 'OK. Block size reduced.'
-    bc = int(size / bs)  # Rude assumption (rounding)
+    block_count = int(size / block_size)  # Rude assumption (rounding)
 
     try:
-        with open(dev_path, 'wb') as f:
+        with open(dev_path, 'wb') as dev_file:
             start_time = time.time()
-            for _ in range(bc):
-                f.write(os.urandom(bs))
+            for _ in range(block_count):
+                dev_file.write(os.urandom(block_size))
             end_time = time.time()
             total_time = end_time - start_time
             speed = size / (total_time * 1024 * 1024)
             LOG.info('Time: %.2f s; Speed: %.2f MB/s' % (total_time, speed))
-    except Exception as e:
+    except IOError as io_err:
         success = 0
-        message = 'ERROR: ' + str(e)
+        message = 'IO ERROR ({0}): {1}'.format(io_err.errno, io_err.strerror)
 
     return {
         'success': success,
         'message': message,
         'dev_path': dev_path,
         'dev_size': size,
-        'bs': bs,
+        'bs': block_size,
         'total_time': '%.2f' % total_time,
         'speed': '%.2f' % speed
     }
 
 
 def main():
+    """ Main. Calls presented logic to run IO sessions as specified in config-file """
+    # pylint: disable=locally-disabled, global-statement
     global LOG
     common_success = True
     args = parse_args()
@@ -104,9 +122,9 @@ def main():
     config = get_json_from_file(args.init_config)
 
     if args.bs:
-        bs = int(args.bs)
+        block_size = int(args.bs)
     else:
-        bs = 4096  # 4Kb
+        block_size = 4096  # 4Kb
 
     if not os.path.isfile(args.dev_list):
         LOG.error('File with list of devices not found')
@@ -129,7 +147,7 @@ def main():
             LOG.warning('More than 1 device with such lun found. Taking first.')
         dev_name = dev_names[0]
 
-        result = write_random('/dev/' + dev_name, int(dev['size']), bs)
+        result = write_random('/dev/' + dev_name, int(dev['size']), block_size)
         result['dev_name'] = dev['name']
         result['dev_type'] = dev['type']
         result['dev_lun'] = dev['lun']
